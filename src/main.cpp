6b6dc3120e54167ea993a793f922934e6a6f2f58
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2023-09-05 01:27:53 +0530

    Initialize the micromouse maze solver project

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
