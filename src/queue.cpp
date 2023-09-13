64b41eb838681f550f205964be90915c90a80a4f
dilumin <77558016+dilumin@users.noreply.github.com>
2023-09-13 18:44:12 +0530

    added queue and stack

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,2 +1,50 @@
 #include"maze.h"
 #include"queue.h"
+
+using namespace std;
+
+bool empty(Queue* queue){
+    return (queue->front == queue->back);
+}
+
+
+Queue* createQueue(int size = DEFAULT_SIZE){
+
+    Queue* queue = new Queue;
+    queue->queue = new Point[size];
+    queue->front = -1;
+    queue->back = -1;
+    queue->size = size;
+
+    return queue;
+}
+
+void deleteQueue(Queue* queue){
+    delete[] queue->queue;
+    delete queue;
+}
+
+
+void resetQueue(Queue* queue){
+    queue->front = -1;
+    queue->back = -1;
+
+}
+
+
+Point dequeue(Queue *q) {
+    if (q->front == q->back) throw -1; // queue is empty
+
+    q->front = (q->front + 1) % q->size;
+    return q->queue[q->front];
+    
+}
+
+
+
+void enqueue(Queue *q, Point value) {
+    if ((q->back + 1) % q->size == q->front) throw -1; // queue is full
+
+    q->back = (q->back + 1) % q->size;
+    q->queue[q->back] = value;
+}
--- src/stack.cpp
+++ src/stack.cpp
@@ -1,2 +1,51 @@
 #include"maze.h"
 #include"stack.h"
+
+using namespace std;
+
+bool empty(Stack* stack){
+    return (stack->top == 0);
+}
+
+
+
+Stack* createStack(int size = DEFAULT_SIZE){
+    
+        Stack* stack = new Stack;
+        stack->stack = new Point[size];
+        stack->top = 0;
+        stack->size = size;
+    
+        return stack;
+
+}
+void deleteStack(Stack* stack){
+    delete[] stack->stack;
+    delete stack;
+}
+
+void resetStack(Stack* stack){
+    stack->top = 0;
+}
+
+Point pop(Stack* stack){
+    if (empty(stack)){
+        return;
+    }
+    else{
+        Point p = stack->stack[ -- (stack->top) ];
+        return p;
+    }
+}
+
+void push(Stack* stack, Point point){
+    if (stack->top == stack->size){
+        
+        return;
+    }
+    else{
+        stack->stack[stack->top ++] = point;
+    }
+}
+
+
