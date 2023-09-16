7e16372b742d95d062974a98d5861b628bd5b66c
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2023-09-16 15:52:17 +0530

    Merege two files main.cpp and TOF and test cpp files

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,26 @@
 #include "maze.h"
 #include "solver.h"
 #include "motors.h"
-#include "TOF.h"
+// #include "TOF.h"
 #include "IR.h"
 #include "user.h"
-#include "test.h"
+// #include "test.h"
 #include "mouse.h"
 
 #include <Arduino.h>
+
+// TOF initialization parameters and variable creations
+// address we will assign if dual sensor is present
+#define LOX1_ADDRESS 0x30
+#define LOX2_ADDRESS 0x31
+#define LOX3_ADDRESS 0x32
+#define LOX4_ADDRESS 0x34
+
+// set the pins to shutdown
+#define SHT_LOX1 2 // FRONT
+#define SHT_LOX2 4 // RIGHT
+#define SHT_LOX3 18 // BACK
+#define SHT_LOX4 19 // LEFT
 
 // create two arrays to represent wall map and flood index map with predefine sizes
 int wallMap[MAZE_SIZE][MAZE_SIZE];
@@ -22,10 +35,128 @@ int pathLength = 0; // length of the path
 int X = 0;
 int Y = 0;
 
+void test() {
+  while (true) {
+    int front = measure1.RangeMilliMeter;
+    if (front > 80) {
+      Serial.printf("Distance (front): %d \n", front);
+      forward(150);
+    } else {
+      int leftD = measure4.RangeMilliMeter;
+      int rightD = measure2.RangeMilliMeter;
+
+     if (leftD > 100) {
+        Serial.printf("Distance (left): %f \n", leftD);
+        left(150);
+        delay(ROTATE_TIME);
+        stop();
+      } else if (rightD < 100) {
+        Serial.printf("Distance (right): %f \n", rightD);
+        right(150);
+        delay(ROTATE_TIME);
+        stop();
+      } else {
+        Serial.printf("Stopped !");
+        stop();
+      }
+    }
+  }
+}
+
 void log(const char* message) {
   #if DEBUG
     Serial.println(message);
   #endif
+}
+
+// objects for the vl53l0x
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox4 = Adafruit_VL53L0X();
+
+// this holds the measurement
+VL53L0X_RangingMeasurementData_t measure1;
+VL53L0X_RangingMeasurementData_t measure2;
+VL53L0X_RangingMeasurementData_t measure3;
+VL53L0X_RangingMeasurementData_t measure4;
+
+void initializeTOF() {
+    
+    pinMode(SHT_LOX1, OUTPUT);
+    pinMode(SHT_LOX2, OUTPUT);
+    pinMode(SHT_LOX3, OUTPUT);
+    pinMode(SHT_LOX4, OUTPUT);
+
+    Serial.println("Shutdown pins inited...");
+
+    digitalWrite(SHT_LOX1, LOW);
+    digitalWrite(SHT_LOX2, LOW);
+    digitalWrite(SHT_LOX3, LOW);
+    digitalWrite(SHT_LOX4, LOW);
+
+    Serial.println("Both in reset mode...(pins are low)");
+}
+
+void setID() {
+    // all reset
+    digitalWrite(SHT_LOX1, LOW);
+    digitalWrite(SHT_LOX2, LOW);
+    digitalWrite(SHT_LOX3, LOW);
+    digitalWrite(SHT_LOX4, LOW);
+    delay(10);
+    // all unreset
+    digitalWrite(SHT_LOX1, HIGH);
+    digitalWrite(SHT_LOX2, HIGH);
+    digitalWrite(SHT_LOX3, HIGH);
+    digitalWrite(SHT_LOX4, HIGH);
+    delay(10);
+
+    // activating LOX1 and reseting LOX2
+    digitalWrite(SHT_LOX1, HIGH);
+    digitalWrite(SHT_LOX2, LOW);
+    digitalWrite(SHT_LOX3, LOW);
+    digitalWrite(SHT_LOX4, LOW);
+
+    // initing LOX1
+    if (!lox1.begin(LOX1_ADDRESS)) {
+        Serial.println(F("Failed to boot first VL53L0X"));
+        while (1)
+        ;
+    }
+    delay(10);
+
+    // activating LOX2
+    digitalWrite(SHT_LOX2, HIGH);
+    delay(10);
+
+    //initing LOX2
+    if (!lox2.begin(LOX2_ADDRESS)) {
+        Serial.println(F("Failed to boot second VL53L0X"));
+        while (1)
+        ;
+    }
+    // activating LOX3
+    digitalWrite(SHT_LOX3, HIGH);
+    delay(10);
+
+    //initing LOX2
+    if (!lox3.begin(LOX3_ADDRESS)) {
+        Serial.println(F("Failed to boot second VL53L0X"));
+        while (1)
+        ;
+    }
+
+    // activating LOX4
+    digitalWrite(SHT_LOX4, HIGH);
+    delay(10);
+
+    //initing LOX2
+    if (!lox4.begin(LOX4_ADDRESS)) {
+        Serial.println(F("Failed to boot second VL53L0X"));
+        while (1)
+        ;
+    }
 }
 
 void setup() {
